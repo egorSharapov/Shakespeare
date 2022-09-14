edc00d67fb1a3fc79d6178ab0feca4ee51fa61f6
egorSharapov <111515657+egorSharapov@users.noreply.github.com>
2022-09-14 08:43:53 +0300

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -14,7 +14,7 @@ void read_from_file (FILE * input_file, char ** string_number, const int count_o
     
     for (int index_of_symbol = 0; index_of_symbol < count_of_symbols; index_of_symbol++)
     {   
-        symbol = fgetc (input_file);
+        symbol = (char) fgetc (input_file);
         my_text[index_of_symbol] = symbol;
 
         if (symbol == '\n')
@@ -25,22 +25,22 @@ void read_from_file (FILE * input_file, char ** string_number, const int count_o
     }
     my_text[count_of_symbols] = '\n';
 
-    fclose(input_file);
+
 }
 
 
 void count_symbols (FILE * input_file, int * count_of_symbols, int * count_of_strings)
 {
     assert (input_file != NULL);
-    assert (count_of_strings > 0);
-    assert (count_of_symbols > 0);
+    assert (count_of_strings != 0);
+    assert (count_of_symbols != 0);
     
     char symbol = '\n';
     (*count_of_strings)++;
 
     while (true)
     {
-        symbol = fgetc(input_file);
+        symbol = (char) fgetc(input_file);
         if (symbol == EOF) break;
         if (symbol == '\n') (*count_of_strings)++;
         (*count_of_symbols)++;
@@ -53,7 +53,7 @@ void count_symbols (FILE * input_file, int * count_of_symbols, int * count_of_st
 void write_to_file (char ** string_number, int count_of_strings, FILE * output_file)
 {
     assert (string_number != NULL);
-    assert (count_of_strings > 0);
+    assert (count_of_strings != 0);
     assert (output_file != NULL);
 
     for (int i = 0; i < count_of_strings; i++)
@@ -75,7 +75,7 @@ void write_to_file (char ** string_number, int count_of_strings, FILE * output_f
 void sort (char ** string_number, int count_of_strings)
 {
     assert (string_number != NULL);
-    assert (count_of_strings > 0);
+    assert (count_of_strings != 0);
 
     for (int i = 0; i < count_of_strings - 1; i++)
     {
@@ -116,16 +116,15 @@ bool string_comparsion (char * string1, char * string2)
 
     if (*temp_string1 == '\n')
         return true;
-    if (*temp_string2 == '\n')
-        return false;
-
+    
+    return false;
 }
 
 
 void reverse_sort (char ** string_number, int count_of_strings, char * my_text)
 {
     assert (string_number != NULL);
-    assert (count_of_strings > 0);
+    assert (count_of_strings != 0);
     assert (my_text != NULL);
 
     reverse_strings (string_number, count_of_strings);
@@ -157,20 +156,21 @@ bool reverse_string_comparsion (char * string1, char * string2)
     
     while (*temp_string1 != '\n' or *temp_string2 != '\n')
     {
+        if (not_letter (*temp_string1)) 
+            temp_string1--;
+        if (not_letter (*temp_string2))
+            temp_string2--;
+        
         if (*temp_string1 > *temp_string2)
             return true;
         else if (*temp_string1-- == *temp_string2--);
-        else if (not_letter (*temp_string1)) 
-            temp_string1--;
-        else if (not_letter (*temp_string2))
-            temp_string2--;
         else
             return false;
     }
     if (*temp_string1 == '\n')
         return true;
-    if (*temp_string2 == '\n')
-        return false;
+    
+    return false;
 }
 
 
@@ -178,7 +178,7 @@ bool reverse_string_comparsion (char * string1, char * string2)
 void reverse_strings (char ** string_number, int count_of_strings)
 {
     assert (string_number != NULL);
-    assert (count_of_strings > 0);
+    assert (count_of_strings != 0);
 
     for (int i = 0; i < count_of_strings; i++)
     {
@@ -186,6 +186,7 @@ void reverse_strings (char ** string_number, int count_of_strings)
             string_number[i]++;
 
         string_number[i]--;
+        printf ("%c\n", *string_number[i]);
     }
 }
 
@@ -193,7 +194,7 @@ void reverse_strings (char ** string_number, int count_of_strings)
 void reverse_indexes (char ** string_number, int count_of_strings, char * my_text)
 {
     assert (string_number != NULL);
-    assert (count_of_strings > 0);
+    assert (count_of_strings != 0);
     assert (my_text != NULL);
 
     for (int i = 0; i < count_of_strings; i++)
--- hamlet.cpp
+++ hamlet.cpp
@@ -15,24 +15,30 @@ int main ()
     }
 
     char * my_text = NULL;
+    char * temp_my_text = NULL;
     int count_of_symbols = 0;
     int count_of_strings = 0;
 
     count_symbols (input_file, &count_of_symbols, &count_of_strings);
 
     my_text = (char *) calloc ((count_of_symbols + 1), sizeof (char));
-    
+
     char *string_number[count_of_strings];
+    char *temp_string_number[count_of_strings];
 
     read_from_file (input_file, string_number, count_of_symbols, my_text);
-    write_to_file (string_number, count_of_strings, output_file);
+    fclose(input_file);
 
+    for (int i = 0; i < count_of_strings; i++)
+        temp_string_number[i] = string_number[i];
+    
     sort (string_number, count_of_strings);
     write_to_file (string_number, count_of_strings, output_file);
 
     reverse_sort (string_number, count_of_strings, my_text);
     write_to_file (string_number, count_of_strings, output_file);
 
+    write_to_file (temp_string_number, count_of_strings, output_file);
     fclose(output_file);
 
     free(my_text);
